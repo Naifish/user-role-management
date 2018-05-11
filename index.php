c12a1c322cae286e33b11095430b737cc6e9ff93
Naifish Ali <nf533051@dal.ca>
2018-05-11 11:24:45 -0300

    Designing home page

--- index.php
+++ index.php
@@ -5,3 +5,18 @@
  * Date: 2018-05-11
  * Time: 11:06 AM
  */
+
+?>
+
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>User role management</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <h1>
+            Welcome to user role management system.
+        </h1>
+    </body>
+</html>
