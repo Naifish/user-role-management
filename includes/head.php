77441efd8ccb8b0886cf465e176b488a9ba6017c
Naifish Ali <nf533051@dal.ca>
2018-05-13 17:44:27 -0300

    header and footer files included

---
+++ includes/head.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User role management</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/normalize.css">
+</head>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Naifish Ali
+ * Date: 2018-05-13
+ * Time: 5:37 PM
+ */
+?>
