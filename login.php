8aa7f88ff3df9b39c628bf6fb907ac1160ce1cf6
Naifish Ali <nf533051@dal.ca>
2018-05-15 10:51:10 -0300

    front-end validation done.

--- login.php
+++ login.php
@@ -15,8 +15,9 @@
     <section class="login-form-sec">
         <h1>Sign In</h1>
         <form action="#" method="post">
-            <span>Email</span><br><input type="email" name="email" placeholder="Email" required>
-            <span>Password</span><br><input type="password" name="password" placeholder="Password" required>
+            <!-- regex for email address is taken from https://www.w3schools.com/tags/att_input_pattern.asp -->
+            <span>Email</span><br><input type="email" name="email" placeholder="Email" required required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Valid email address is required">
+            <span>Password</span><br><input type="password" name="password" placeholder="Password" required required pattern=".{8,}" title="Eight or more characters are required">
             <br><input type="submit" name="btn-login" value="Login"><p><a href="registration.php">Create Account</a> | <a href="#">Forgot Password</a> </p>
         </form>
     </section>
--- registration.php
+++ registration.php
@@ -14,19 +14,34 @@
             <section class="registration-form-sec">
                 <h1>Sign Up</h1>
                 <form action="#" method="post">
-                    <span>First Name</span><br><input type="text" name="firstName" placeholder="First Name" required>
-                    <span>Last Name</span><br><input type="text" name="lastName" placeholder="Last Name" required>
-                    <span>Email</span><br><input type="email" name="email" placeholder="Email" required>
-                    <span>Password</span><br><input type="password" name="password" placeholder="Password" required>
-                    <span>Confirm Password</span><br><input type="password" name="confrm-password" placeholder="Confirm Password" required>
-                    <span>Street Address</span><br><input type="text" name="street" placeholder="Street Address" required>
-                    <span>Postal Code</span><br><input type="text" name="postal" placeholder="Postal Code" required>
+                    <span>First Name</span><br><input type="text" name="firstName" placeholder="First Name" required pattern="^[A-Za-z]+$" title="Only letters accepted">
+                    <span>Last Name</span><br><input type="text" name="lastName" placeholder="Last Name" required required pattern="^[a-z '-]+$" title="Only letters, space, hyphen and apostrophe are accepted ">
+                    <!-- regex for email address is taken from https://www.w3schools.com/tags/att_input_pattern.asp -->
+                    <span>Email</span><br><input type="email" name="email" placeholder="Email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Valid email address is required">
+                    <span>Password</span><br><input type="password" id="pass" name="pass" placeholder="Password" required pattern=".{8,}" title="Eight or more characters are required">
+                    <span>Confirm Password</span><br><input type="password" id="conf-pass" name="conf-pass" placeholder="Confirm Password" required>
+                    <span>Street Address</span><br><input type="text" name="street" placeholder="Street Address" pattern="^\d+\s[A-z, \d]+" title=" eg: 245 Harlington street Halifax, NS B3M 1RC, Canada">
+                    <span>Postal Code</span><br><input type="text" name="postal" placeholder="Postal Code" pattern="^[A-z, \d]{3}\s[A-z, \d]{3}$" title="Numbers and letters accepted only with one space. eg: XXX XXX">
                     <br><input type="submit" name="btn-submit" value="Create my account">
                     <p>Already a member? <a href="login.php">Sign In</a> </p>
                 </form>
             </section>
         </div>
         <?php include 'includes/footer.php'?>
+        <!-- Reference: https://codepen.io/diegoleme/pen/surIK -->
+        <script type="text/javascript">
+            var pass = document.getElementById("pass")
+                , confPass = document.getElementById("conf-pass");
+            function matchPassword(){
+                if(pass.value != confPass.value) {
+                    confPass.setCustomValidity("Passwords Don't Match");
+                } else {
+                    confPass.setCustomValidity('');
+                }
+            }
+            pass.onchange = matchPassword;
+            confPass.onkeyup = matchPassword;
+        </script>
     </body>
 </html>
 
