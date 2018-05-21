eb4fb221d19a36da0a646aa8a7dcc99f1642b144
Naifish Ali <nf533051@dal.ca>
2018-05-21 15:21:03 -0300

    Additional layer of security added

--- registration.php
+++ registration.php
@@ -24,19 +24,19 @@ if (isset($_POST['btn-submit'])){
         $errors[]="First Name is required";
     }elseif (!(preg_match('/^[A-Za-z]+$/',$_POST['firstName']))){
         $errors[]="Invalid first name: Only letters are accepted";
-    }else{ $user->_set('firstName',$_POST['firstName']); }
+    }else{ $user->_set('firstName',checkInput($_POST['firstName'])); }
 
     if (empty($_POST['lastName'])){
         $errors[]="Last Name is required";
     }elseif (!(preg_match('/^[A-Za-z \'-]+$/',$_POST['lastName']))){
         $errors[]="Invalid last name: Only letters, space, hyphen and apostrophe are accepted";
-    }else{ $user->_set('lastName',$_POST['lastName']); }
+    }else{ $user->_set('lastName',checkInput($_POST['lastName'])); }
 
     if (empty($_POST['email'])){
         $errors[]="Email is required";
     }elseif (!(preg_match('/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/',$_POST['email']))){
         $errors[]="Invalid email address: Valid email address is required";
-    }else{ $user->_set('email',$_POST['email']); }
+    }else{ $user->_set('email',checkInput($_POST['email'])); }
 
     if (empty($_POST['pass'])){
         $errors[]="Password is required";
@@ -46,22 +46,22 @@ if (isset($_POST['btn-submit'])){
         $options = [
             'cost' => 12,
         ];
-        $user->_set('pass',password_hash($_POST['pass'], PASSWORD_BCRYPT, $options));
+        $user->_set('pass',password_hash(checkInput($_POST['pass']), PASSWORD_BCRYPT, $options));
     }
 
     if (empty($_POST['confPass'])){
         $errors[]="Confirmation password is required";
     }elseif ($_POST['pass']!=$_POST['confPass']){
         $errors[]="Invalid confirmation password: Passwords Don't Match";
-    }else{ $user->_set('confPass',$_POST['confPass']); }
+    }else{ $user->_set('confPass',checkInput($_POST['confPass'])); }
 
     if (!empty($_POST['street']) && !(preg_match('/^\d+\s[A-z, \d]+$/',$_POST['street']))){
         $errors[]="Invalid street address: eg: 245 Harlington street Halifax, NS B3M 1RC, Canada";
-    }else{ $user->_set('street',$_POST['street']); }
+    }else{ $user->_set('street',checkInput($_POST['street'])); }
 
     if (!empty($_POST['postal']) && !(preg_match('/^[A-z, \d]{3}\s[A-z, \d]{3}$/',$_POST['postal']))){
         $errors[]="Invalid postal code: Numbers and letters accepted only with one space. eg: XXX XXX";
-    }else{ $user->_set('postal',$_POST['postal']); }
+    }else{ $user->_set('postal',checkInput($_POST['postal'])); }
 
     $arrLength=count($errors);
 
@@ -118,6 +118,14 @@ if (isset($_POST['btn-submit'])){
 }
 
 
+/* Reference: https://www.w3schools.com/php/php_form_validation.asp */
+function checkInput($val) {
+    $val = trim($val);
+    $val = stripslashes($val);
+    $val = htmlspecialchars($val);
+    return $val;
+}
+/* End Reference */
 
 ?>
 <!DOCTYPE html>
@@ -138,14 +146,14 @@ if (isset($_POST['btn-submit'])){
                 </section>
                 <?php }?>
                 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-                    <span>First Name</span><br><input type="text" value="<?php if (isset($_POST['firstName'])){echo $_POST['firstName'];} ?>" name="firstName" placeholder="First Name" required pattern="^[A-Za-z]+$" title="Only letters accepted">
-                    <span>Last Name</span><br><input type="text" value="<?php if (isset($_POST['lastName'])){echo $_POST['lastName'];} ?>" name="lastName" placeholder="Last Name" required required pattern="^[A-Za-z '-]+$" title="Only letters, space, hyphen and apostrophe are accepted">
+                    <span>First Name</span><br><input type="text" value="<?php if (isset($_POST['firstName'])){echo htmlentities($_POST['firstName']);} ?>" name="firstName" placeholder="First Name" required pattern="^[A-Za-z]+$" title="Only letters accepted">
+                    <span>Last Name</span><br><input type="text" value="<?php if (isset($_POST['lastName'])){echo htmlentities($_POST['lastName']);} ?>" name="lastName" placeholder="Last Name" required required pattern="^[A-Za-z '-]+$" title="Only letters, space, hyphen and apostrophe are accepted">
                     <!-- regex for email address is taken from https://www.w3schools.com/tags/att_input_pattern.asp -->
-                    <span>Email</span><br><input type="email" value="<?php if (isset($_POST['email'])){echo $_POST['email'];} ?>" name="email" placeholder="Email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Valid email address is required">
-                    <span>Password</span><br><input type="password" value="<?php if (isset($_POST['pass'])){echo $_POST['pass'];} ?>" id="pass" name="pass" placeholder="Password" required pattern=".{8,}" title="Eight or more characters are required">
-                    <span>Confirm Password</span><br><input type="password" value="<?php if (isset($_POST['confPass'])){echo $_POST['confPass'];} ?>" id="conf-pass" name="confPass" placeholder="Confirm Password" required>
-                    <span>Street Address</span><br><input type="text" value="<?php if (isset($_POST['street'])){echo $_POST['street'];} ?>" name="street" placeholder="Street Address" pattern="^\d+\s[A-z, \d]+" title=" eg: 245 Harlington street Halifax, NS B3M 1RC, Canada">
-                    <span>Postal Code</span><br><input type="text" value="<?php if (isset($_POST['postal'])){echo $_POST['postal'];} ?>" name="postal" placeholder="Postal Code" pattern="^[A-z, \d]{3}\s[A-z, \d]{3}$" title="Numbers and letters accepted only with one space. eg: XXX XXX">
+                    <span>Email</span><br><input type="email" value="<?php if (isset($_POST['email'])){echo htmlentities($_POST['email']);} ?>" name="email" placeholder="Email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Valid email address is required">
+                    <span>Password</span><br><input type="password" value="<?php if (isset($_POST['pass'])){echo htmlentities($_POST['pass']);} ?>" id="pass" name="pass" placeholder="Password" required pattern=".{8,}" title="Eight or more characters are required">
+                    <span>Confirm Password</span><br><input type="password" value="<?php if (isset($_POST['confPass'])){echo htmlentities($_POST['confPass']);} ?>" id="conf-pass" name="confPass" placeholder="Confirm Password" required>
+                    <span>Street Address</span><br><input type="text" value="<?php if (isset($_POST['street'])){echo htmlentities($_POST['street']);} ?>" name="street" placeholder="Street Address" pattern="^\d+\s[A-z, \d]+" title=" eg: 245 Harlington street Halifax, NS B3M 1RC, Canada">
+                    <span>Postal Code</span><br><input type="text" value="<?php if (isset($_POST['postal'])){echo htmlentities($_POST['postal']);} ?>" name="postal" placeholder="Postal Code" pattern="^[A-z, \d]{3}\s[A-z, \d]{3}$" title="Numbers and letters accepted only with one space. eg: XXX XXX">
                     <br><input type="submit" name="btn-submit" value="Create my account">
                     <p>Already a member? <a href="login.php">Sign In</a> </p>
                 </form>
